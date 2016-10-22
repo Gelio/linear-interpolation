335ce974ae52b044c979ba7a799e4850e394a7c4
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2016-10-22 10:15:10 +0200

    fix function and file names, clean up files

--- calculateCoefficientsSingleSquare.m
+++ calculateCoefficientsSingleSquare.m
@@ -1,22 +1,28 @@
 % interpolacja na kwadracie (dzieli kwadrat na dwa trojkaty przystajace)
-% (x0, y0) - lewy gorny rog kwadratu
+% (x0, y0) - lewy dolny rog kwadratu
 % coefficients - wspolczynniki do funkcji interpolujacej:
 %   - na trojkacie majacym kat prosty w lewym gornym rogu kwadratu
 %   - na trojkacie majacym kat prosty w prawym dolnym rogu kwadratu
 
-function coefficients = interpolateSingleSquare(f, x0, y0, h)
+function coefficients = calculateCoefficientsSingleSquare(f, x0, y0, h)
 coefficients = zeros(2, 3);
+
+topLeft = f(x0, y0+h);
+topRight = f(x0+h, y0+h);
+bottomLeft = f(x0, y0);
+bottomRight = f(x0+h, y0);
+
 
 % Lewy gorny trojkat
 A1 = [
     1  x0  y0;
-    1 x0+h y0;
-    1  x0  y0-h
+    1 x0+h y0+h;
+    1  x0  y0+h
     ];
 b1 = [
-    f(x0, y0);
-    f(x0+h, y0);
-    f(x0, y0-h)
+    bottomLeft;
+    topRight;
+    topLeft
     ];
 
 coefficients(1, :) = reshape(linsolve(A1, b1), 1, 3);
@@ -24,14 +30,14 @@ coefficients(1, :) = reshape(linsolve(A1, b1), 1, 3);
 
 % Prawy dolny trojkat
 A2 = [
-    1  x0  y0-h;
-    1 x0+h y0-h;
-    1  x0+h  y0
+    1  x0  y0;
+    1 x0+h y0;
+    1 x0+h y0+h
     ];
 b2 = [
-    f(x0, y0-h);
-    f(x0+h, y0-h);
-    f(x0+h, y0)
+    bottomLeft;
+    bottomRight;
+    topRight
     ];
 
 coefficients(2, :) = reshape(linsolve(A2, b2), 1, 3);
--- calculateCoefficientsSquare.m
+++ calculateCoefficientsSquare.m
@@ -1,10 +1,10 @@
-function coefficients = interpolateSquare(f, x0, y0, H, n)
+function coefficients = calculateCoefficientsSquare(f, x0, y0, H, n)
 coefficients = zeros(n, n, 2, 3);
 h = H / n;
 
 for i=1:n
     for j=1:n
-        singleSquareCoefficients = interpolateSingleSquare(f, x0 + j*h, y0 - i*h, h);
+        singleSquareCoefficients = calculateCoefficientsSingleSquare(f, x0 + j*h, y0 + i*h, h);
         coefficients(i, j, 1, :) = singleSquareCoefficients(1, :);
         coefficients(i, j, 2, :) = singleSquareCoefficients(2, :);
     end
